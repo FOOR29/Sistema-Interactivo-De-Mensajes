712d67392b103719e63946226493d4277cd5111a
Forlan Ordoñez <nalrofor@gmail.com>
2025-06-20 18:14:19 -0500

    feat: logica de js agregada

--- src/Js/main.js
+++ src/Js/main.js
@@ -1,2 +1,24 @@
-let message 
+const button = document.getElementById('button')
+const result = document.getElementById('result')
+
+button.addEventListener("click", function() {
+    let name = document.getElementById('name').value
+    let age = document.getElementById('age').value
+
+    let message;
+
+    age = parseInt(age);
+
+    if (isNaN(age)) {
+        message = 'Error: por favor ingrese una edad validad'
+    } else if (age < 18) {
+        message = `Hola ${name}, eres menor de edad`
+    } else if (age >= 18 && age <= 59) {
+        message = `Hola ${name}, eres mayor de edad`
+    } else if (age >= 60) {
+        message = `Hola ${name}, eres adulto mayor`
+    }
+
+    result.textContent = message
+} )
 
